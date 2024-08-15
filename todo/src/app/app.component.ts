106b2aedc73cbd1710974014e24892095fe64ec4
capchy <prashantchoudhary2003.pc@gmail.com>
2024-08-15 23:43:31 +0530

    functioning todo app

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodobarComponent } from './todobar/todobar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TodobarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- todo/src/app/todobar/todobar.component.ts
+++ todo/src/app/todobar/todobar.component.ts
@@ -1,12 +1,28 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todobar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,NgFor,NgIf],
   templateUrl: './todobar.component.html',
   styleUrl: './todobar.component.css'
 })
 export class TodobarComponent {
+  taskarray =[{taskname:'brush teeth',iscomplete: false}]
+  load(taskform:NgForm) {
+    this.taskarray.push({taskname:taskform.controls['task'].value, iscomplete:false})
+    
+    taskform.reset();
+  }
+  delete(i:number){
+    // console.log(i);
+    this.taskarray.splice(i,1);
+    }
+  oncheck(i:number){
+    this.taskarray[i].iscomplete=!this.taskarray[i].iscomplete
 
+  }
+  
 }
